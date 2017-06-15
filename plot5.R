ce8cc7a4e1548f976b7c53aa16d04103a0e6601f
Julius <medicjue@gmail.com>
2017-06-15 23:49:38 +0800

    Modify plot5 and 6

--- plot5.R
+++ plot5.R
@@ -3,14 +3,16 @@ source("loadData.R")
 
 source("readData.R")
 
+library(plyr)
+library(ggplot2)
+
 motorSCC <- SCC[grepl('Motor', SCC$Short.Name, fixed=T), ]
 motorEMI <- merge(x=EMI, y=motorSCC, by="SCC")
 motorBalEMI <- subset(motorEMI, motorEMI$fips=="24510")
 
 aggMotorBalEMI <- aggregate(Emissions ~ year, motorBalEMI, sum)
 
-
-ggplot(data = aggMotorBalEMI, aes(x=year, y=Emissions)) + geom_line() + geom_text(aes(label=Emissions), vjust=1)+ geom_point( size=4, shape=21, fill="white") + xlab("Year") + ylab("Emissions (thousands of tons)" ) + ggtitle("Baltimore Emissions from Motorcycle")
+ggplot(data = aggMotorBalEMI, aes(x=year, y=Emissions)) + geom_line() + geom_text(aes(label=Emissions), vjust=-2)+ geom_point( size=4, shape=21, fill="white") + xlab("Year") + ylab("Emissions (thousands of tons)" ) + ggtitle("Baltimore Emissions from Motorcycle")
 
 dev.copy(png, filename="plot5.png", width=480, height=480)
 dev.off ()
--- plot6.R
+++ plot6.R
@@ -3,12 +3,17 @@ source("loadData.R")
 
 source("readData.R")
 
-motorSCC <- SCC[grepl('Motor', SCC$Short.Name, fixed=T), ]
-motorEMI <- merge(x=EMI, y=motorSCC, by="SCC")
-motorBalAndLAEMI <- subset(motorEMI, motorEMI$fips=="24510" | motorEMI$fips=="06037")
-motorLAEMI <-  subset(motorEMI, motorEMI$fips=="06037")
+library(plyr)
+library(ggplot2)
 
-aggMotorBalAndLAEMI <- aggregate(Emissions ~ year+fips, motorBalAndLAEMI, FUN = sum)
+motorSCC <- SCC[grepl('vehicle', SCC$SCC.Level.Two, ignore.case=T), ]
+motorEMI <- merge(x=EMI, y=motorSCC, by="SCC")
+motorBalEMI <- subset(motorEMI, motorEMI$fips=="24510" )
+motorLAEMI <-  subset(motorEMI, motorEMI$fips=="06037")
+motorBalEMI$city <- "Baltimore"
+motorLAEMI$city <- "Los Angeles"
+
+aggMotorBalAndLAEMI <- aggregate(Emissions ~ year+city, motorBalAndLAEMI, sum)
 
 ggplot(data=motorBalAndLAEMI, aes(x=year, y=Emissions, group=fips, color=fips)) + geom_line() + geom_point( size=4, shape=21, fill="white") + xlab("Year") + ylab("Emissions (tons)") + ggtitle("Motorcycle Related PM2.5 Emissions in Baltimore & Los Angeles (1999 - 2008)")
 
